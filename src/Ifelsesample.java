ec0e31051d78dba2a7bec8bd39cccb78bb4ecfe1
jjancychristy <jjancychristy24081988@gmail.com>
2023-01-03 19:34:22 -0500

    first commit

---
+++ src/Ifelsesample.java
@@ -0,0 +1,22 @@
+
+public class Ifelsesample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		if(b==a)
+		{
+			System.out.println("b is greater");
+		}
+		else if(b<a)
+		{
+			System.out.println("b is correct");
+			}
+		else
+		{
+			System.out.println(b);
+		}
+	}
+
+}
---
+++ src/Stringexample.java
@@ -0,0 +1,28 @@
+
+public class Stringexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name="Christy";
+		String blank=" ";
+		String text= "Hello";
+        System.out.println(text+blank+name);	
+
+	System.out.println("Hello Jancy Christy Johnson");
+    int a=10;
+    System.out.println(a);
+    boolean b= false;
+    System.out.println(b);
+    b= true;
+    char c='i';
+    System.out.println(c);
+
+    String d="Welcome To Java";
+    System.out.println(d);
+    double e=10.5;
+    System.out.println(e);
+    System.out.println(b);
+    System.out.println(b);
+}
+}
